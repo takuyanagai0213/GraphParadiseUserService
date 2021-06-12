4bb1acfe36060a3bdadfc137f360f6173645d3a9
takuya <nagataku021@outlook.jp>
2021-06-12 12:31:19 +0900

    最低限のapiを作成

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -8,10 +9,19 @@ import (
 func main() {
 	// controller
 	http.HandleFunc("/", echoHello)
+	http.HandleFunc("/getData", getData)
 	// port
 	http.ListenAndServe(":8000", nil)
 }
 
 func echoHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello World</h1>")
+}
+
+func getData(w http.ResponseWriter, r *http.Request) {
+	data := []int{10, 20, 20, 15, 40, 100}
+
+	// fmt.Fprintf(w, "<h1>data</h1>")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
 }
