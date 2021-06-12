895763caff43dffb6869c8a0376b772ac7b836b9
takuya <nagataku021@outlook.jp>
2021-06-12 23:29:49 +0900

    グラフを表示

--- main.go
+++ main.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	// controller
+	dir, _ := os.Getwd() //追記
+	log.Print(http.Dir(dir + "/static/"))
 	http.HandleFunc("/", echoHello)
 	http.HandleFunc("/getData", getData)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(dir+"/static/")))) //追記
 	// port
 	http.ListenAndServe(":3000", nil)
 }
 
 func echoHello(w http.ResponseWriter, r *http.Request) {
+	log.Print("api/")
 	t, err := template.ParseFiles("index.html")
 	if err != nil {
 		panic(err.Error())
@@ -28,7 +33,6 @@ func echoHello(w http.ResponseWriter, r *http.Request) {
 func getData(w http.ResponseWriter, r *http.Request) {
 	data := []int{10, 20, 20, 15, 40, 100}
 
-	fmt.Fprintf(w, "<h1>data</h1>")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
 }
---
+++ static/main.js
@@ -0,0 +1,69 @@
+console.log('www')
+class chart {
+  constructor(json){
+    console.log(this.json)
+  }
+  drowChart(json){
+    console.log(json)
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+      labels: ['8月1日', '8月2日', '8月3日', '8月4日', '8月5日', '8月6日', '8月7日'],
+      datasets: [
+        {
+          label: '最高気温(度）',
+          data: json,
+          borderColor: "rgba(255,0,0,1)",
+          backgroundColor: "rgba(0,0,0,0)"
+        },
+        {
+          label: '最高気温(度）',
+          data: json,
+          borderColor: "rgba(255,0,0,1)",
+          backgroundColor: "rgba(0,0,0,0)"
+        },
+        {
+          label: '最高気温(度）',
+          data: json,
+          borderColor: "rgba(255,0,0,1)",
+          backgroundColor: "rgba(0,0,0,0)"
+        },
+        {
+          label: '最低気温(度）',
+          data: [25, 27, 27, 25, 26, 27, 25, 21],
+          borderColor: "rgba(0,0,255,1)",
+          backgroundColor: "rgba(0,0,0,0)"
+        }
+      ],
+    },
+    options: {
+      title: {
+        display: true,
+        text: '気温（8月1日~8月7日）'
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            suggestedMax: 40,
+            suggestedMin: 0,
+            stepSize: 10,
+            callback: function(value, index, values){
+              return  value +  '度'
+            }
+          }
+        }]
+      },
+    }
+    });
+  }
+}
+const chartClass = new chart();
+$.ajax({
+  url: '/getData',
+  type: "get",
+  dataType: 'json',
+}).then(function (json) {
+  console.log(json)
+  chartClass.drowChart(json);
+});
