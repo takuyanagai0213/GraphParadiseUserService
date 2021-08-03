1136ce91ac256de6067b9de74c991e0827d4ec85
takuya <nagataku021@outlook.jp>
2021-08-03 22:27:23 +0900

    terraformのファイルを追加

---
+++ terraform/test.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+  access_key = "${AWS_ACCESS_KEY}"
+  secret_key = "${AWS_SECRET_KEY}"
+}
