540645c4583218097a8877f82e45c6f8ab62ec4f
takuya <nagataku021@outlook.jp>
2021-08-24 21:26:45 +0900

    jsとhtml追加

--- users/main.go
+++ users/main.go
@@ -12,6 +12,7 @@ func main() {
 	dir, _ := os.Getwd()
 
 	http.HandleFunc("/users", users)
+	http.HandleFunc("/users/all", user_list)
 	http.HandleFunc("/user/new", api.CreateUser)
 	http.HandleFunc("/user/get", api.GetUsers)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(dir+"/static/"))))
@@ -21,6 +22,15 @@ func main() {
 
 func users(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("users.html")
+	if err != nil {
+		panic(err.Error())
+	}
+	if err := t.Execute(w, nil); err != nil {
+		panic(err.Error())
+	}
+}
+func user_list(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("user_list.html")
 	if err != nil {
 		panic(err.Error())
 	}
