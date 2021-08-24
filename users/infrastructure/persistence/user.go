692c9df619ee73287c3f672f9e3001aecc03ec15
takuya <nagataku021@outlook.jp>
2021-08-24 22:10:05 +0900

    usecaseを追加

--- users/infrastructure/persistence/user.go
+++ users/infrastructure/persistence/user.go
@@ -3,6 +3,7 @@ package persistence
 import (
 	"github.com/jinzhu/gorm"
 
+	"github.com/sample/sample-api/domain/model"
 	"github.com/sample/sample-api/domain/repository"
 )
 
@@ -14,4 +15,23 @@ type userPersistence struct {
 // Userデータに関するPersistenceを生成
 func NewUserPersistence(conn *gorm.DB) repository.UserRepository {
 	return &userPersistence{Conn: conn}
+}
+
+// 検索
+func (up *userPersistence) Search(name string) ([]*model.User, error) {
+	var user []model.User
+
+	// DB接続確認
+	if err := up.Conn.Take(&user).Error; err != nil {
+		return nil, err
+	}
+
+	db := up.Conn.Find(&user)
+
+	// 名前検索
+	if name != "" {
+		db = db.Where("name = ?", name).Find(&user)
+	}
+
+	return user, nil
 }
---
+++ users/usecase/user.go
@@ -0,0 +1,30 @@
+package usecase
+
+import (
+	"github.com/sample/sample-api/domain/repository"
+)
+
+// UserにおけるUseCaseのインターフェース
+type UserUseCase interface {
+	Search(name string) ([]*model.User, error)
+}
+
+type userUseCase struct {
+	userRepository repository.UserRepository
+}
+
+// Userデータに関するUseCaseを生成
+func NewUserUseCase(ur repository.UserRepository) UserUseCase {
+	return &userUseCase{
+		userRepository: ur,
+	}
+}
+
+// 検索
+func (uu userUseCase) Search(name string) (user []*model.User, err error) {
+	user, err = uu.userRepository.Search(name)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
