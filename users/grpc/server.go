6def9bdd2c89c733d8f686bac5af49cb336e9061
takuya <nagataku021@outlook.jp>
2021-08-31 21:31:06 +0900

    server.go修正

--- users/grpc/server.go
+++ users/grpc/server.go
@@ -1,87 +1,63 @@
 package grpc
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 
-	"github.com/takuyanagai0213/GraphParadiseUserService/domain/model"
-	"github.com/takuyanagai0213/GraphParadiseUserService/grpc/userservice"
-	"github.com/takuyanagai0213/GraphParadiseUserService/usecase"
+	"github.com/yzmw1213/UserService/grpc/userservice"
+	"github.com/yzmw1213/UserService/usecase/interactor"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type server struct {
-	Usecase usecase.UserUseCase
+	Usecase interactor.UserInteractor
 }
 
+// NewUserGrpcServer gRPCサーバー起動
 func NewUserGrpcServer() {
-
-	fmt.Println("Blog Service Started")
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
-	// grpc
-	opts := []grpc.ServerOption{}
-	s := grpc.NewServer(opts...)
+	server := &server{}
 
-	userservice.RegisterUserServiceServer(s, &server{})
+	s := makeServer()
+
+	userservice.RegisterUserServiceServer(s, server)
+
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	log.Println("main grpc server has started")
+
 	go func() {
-		fmt.Println("Starting server ....")
 		if err := s.Serve(lis); err != nil {
-			log.Fatalf("Failed to serve: %v", err)
+			log.Fatalf("failed to serve: %v", err)
 		}
 	}()
-	//Wait for Control C to Exit
+
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt)
 
+	// Block until a sgnal is received
 	<-ch
 	fmt.Println("Stopping the server")
 	s.Stop()
-	fmt.Println("Close the listener")
+	fmt.Println("Closing the client")
 	lis.Close()
-	fmt.Println("end of program")
-}
-func (s server) ReadUser(ctx context.Context, req *userservice.ReadUserRequest) (*userservice.ReadUserResponse, error) {
-	name := req.GetName()
-	fmt.Println(name)
-	user_data, err := s.Usecase.GetUserByName(name)
+	fmt.Println("End of Program")
 
-	if err != nil {
-		return nil, err
-	}
-	user_data_comp := makeGrpcUserProfile(&user_data)
-	return &userservice.ReadUserResponse{
-		User: user_data_comp,
-	}, nil
 }
-func makeGrpcUserProfile(user_data *model.User) *userservice.User {
-	gUser := &userservice.User{
-		Name:     user_data.Name,
-		Password: user_data.Password,
-		Area:     "a",
-	}
-	return gUser
-}
-func (s server) ListUser(ctx context.Context, req *userservice.ListUserRequest) (*userservice.ListUserResponse, error) {
-	rows, err := s.Usecase.List()
-	if err != nil {
-		return nil, err
-	}
-	var users []*userservice.User
-	for _, user := range rows {
-		user := makeGrpcUserProfile(&user)
-		users = append(users, user)
-	}
-	res := &userservice.ListUserResponse{
-		User: users,
-	}
 
-	return res, nil
+func makeServer() *grpc.Server {
+	s := grpc.NewServer(
+		grpc.UnaryInterceptor(grpc.UnaryServerInterceptor(transmitStatusInterceptor)),
+		// grpc_auth.UnaryServerInterceptor(authorization.AuthFunc),
+		// ),
+	)
+	return s
 }
